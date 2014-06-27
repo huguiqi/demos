1b93f32dc377fe39e3e8f5c50fbf7f41a54c0452
huguiqi <guiqi.hu@163.com>
2014-06-27 12:31:11 +0800

    first commit

---
+++ WFViewController.h
@@ -0,0 +1,13 @@
+//
+//  WFViewController.h
+//  demoKits
+//
+//  Created by 胡 桂祁 on 6/27/14.
+//  Copyright (c) 2014 Jinjiang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFViewController : UIViewController
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demoKits
+//
+//  Created by 胡 桂祁 on 6/27/14.
+//  Copyright (c) 2014 Jinjiang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WFAppDelegate class]));
+    }
+}
